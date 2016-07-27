64e9d7f99e2add18a2f02dbe636bae2901c65c27
katsu kouhou <kouhou@inside.ai>
2016-07-27 12:09:25 +0900

    Modify

--- ANNWrapperLibrary.py
+++ ANNWrapperLibrary.py
@@ -1,13 +1,22 @@
 # _*_ coding= utf-8 _*_
 import cv2
 import numpy as np
-import glob
-from random import randint
 
-"""
-ANNを作成する
-"""
-def createANN(layer_sizes):
+
+def create_ann(layer_sizes):
+    """ANNを生成する
+
+        ANNモデルのインスタンスを作成する
+
+        パラメータ:
+            layer_sizes: ANNモデルのレイヤーサイズ
+
+        戻り値:
+            ANNインスタンス
+
+        例外:
+            なし
+    """
     # ANNインスタンスを生成する
     ann = cv2.ANN_MLP()
     # ANNのレイヤーを設定する
@@ -16,10 +25,22 @@ def createANN(layer_sizes):
     return ann
 
 
-"""
-学習を実施する
-"""
 def train(ann, train_data, train_resp):
+    """学習を実施する
+
+            ANN方式で学習データを使って学習を実施する
+
+            パラメータ:
+                ann: ANNモデルのインスタンス
+                train_data: 学習データ
+                train_resp: 学習データに対応する結果
+
+            戻り値:
+                ANNインスタンス
+
+            例外:
+                なし
+    """
     # Set criteria
     criteria = (cv2.TERM_CRITERIA_COUNT | cv2.TERM_CRITERIA_EPS, 500, 0.0001)
     # Set parameters
@@ -33,10 +54,21 @@ def train(ann, train_data, train_resp):
     return ann
 
 
-"""
-評価を実施する
-"""
 def evaluate(ann, test_data):
+    """評価を実施する
+
+            評価データを使って評価を実施する。評価用画像を表示し、予測結果を出力する
+
+            パラメータ:
+                ann: ANNモデルのインスタンス
+                test_data: 評価用データ
+
+            戻り値:
+                なし
+
+            例外:
+                なし
+    """
     # サンプルデータを取得する
     sample_data = np.array(test_data[0][0].ravel(), dtype=np.float32)
     # 画像に変換する
@@ -48,10 +80,21 @@ def evaluate(ann, test_data):
     print ann.predict(sample_data)
 
 
-"""
-予測を実施する
-"""
 def predict(ann, sample_image):
+    """予測を実施する
+
+            サンプルデータを使って予測を実施する。予測結果を出力する
+
+            パラメータ:
+                ann: ANNモデルのインスタンス
+                test_data: 評価用データ
+
+            戻り値:
+                なし
+
+            例外:
+                なし
+    """
     # 対象画像を取得する
     target_image = sample_image.copy()
     # 画像サイズを標準化する
@@ -60,35 +103,3 @@ def predict(ann, sample_image):
         target_image = cv2.resize(target_image, (28, 28), interpolation=cv2.INTER_CUBIC)
     # 予測結果を返す
     return ann.predict(np.array([target_image.ravel()], dtype=np.float32))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
